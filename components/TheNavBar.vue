f79d13878bae92289a44aaf20c5e39875ef3e28d
Karn <mail@karngyan.com>
2021-06-13 06:08:33 +0530

    understanding the internet

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -1,5 +1,5 @@
 <template>
-<nav class="backdrop-filter backdrop-blur shadow-sm sticky top-0 z-40 w-full">
+<nav class="backdrop-filter backdrop-blur sticky top-0 z-40 w-full">
   <div class="max-w-7xl border-l border-r border-dashed border-gray-700 mx-auto px-2 sm:px-4 lg:px-8">
     <div class="relative flex items-center justify-between h-16">
       <div class="flex items-center px-2 lg:px-0">
